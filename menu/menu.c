6639a17cd92ca5859b2f0e93ec6a4f9989308330
root <vinidoggll@hotmail.com>
2013-09-23 02:11:41 -0300

    Corrigido o código de interação com os botões da tela.

--- menu/menu.c
+++ menu/menu.c
@@ -1,6 +1,7 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 #include <stdio.h>
+
 // Atributos da tela
 const int LARGURA_TELA = 800;
 const int ALTURA_TELA = 600;
@@ -12,6 +13,7 @@ int main(void){
     ALLEGRO_BITMAP *fundo = NULL, *start = NULL, *sair = NULL;
     // Variável representando interação de eventos
     ALLEGRO_EVENT_QUEUE *interacao = NULL;
+
     // Inicializa a Allegro
     al_init();
     // Inicializa o add-on para utilização de imagens
@@ -20,26 +22,31 @@ int main(void){
     janela = al_create_display(LARGURA_TELA, ALTURA_TELA);
     // Configura o título da janela
     al_set_window_title(janela, "AMNÉSIA!");
+
     // Torna apto o uso de mouse na aplicação
     if (!al_install_mouse()){
         fprintf(stderr, "Falha ao inicializar o mouse.\n");
         al_destroy_display(janela);
         return -1;
     }
+
     // Atribui o cursor padrão do sistema para ser usado
     if (!al_set_system_mouse_cursor(janela, ALLEGRO_SYSTEM_MOUSE_CURSOR_DEFAULT)){
         fprintf(stderr, "Falha ao atribuir ponteiro do mouse.\n");
         al_destroy_display(janela);
         return -1;
     }
+
     // Carrega a imagem
     fundo = al_load_bitmap("fundo.png");
     start = al_load_bitmap("start.png");
     sair = al_load_bitmap("sair.png");
+
     // Desenha a imagem na tela
     al_draw_bitmap(fundo, 0, 0, 0);    //x, y, z(inverte imagem)
     al_draw_bitmap(start, 255, 0, 0); //x, y, z(inverte imagem)
     al_draw_bitmap(sair, 380, 400, 0); //x, y, z(inverte imagem)
+   
     // Atualiza a tela
     al_flip_display();
 
@@ -50,43 +57,51 @@ int main(void){
         return -1;
     }
 
-    // Inicialização das interacoes do mouse
+    // Inicialização das interações do mouse
     al_register_event_source(interacao, al_get_mouse_event_source());
- 
+    
     int na_start= 0;
-    while (!sair){
-        // Verificacao das interacoes do mouse
-        while (!al_is_event_queue_empty(interacao)){
-            ALLEGRO_EVENT evento;
-            al_wait_for_event(interacao, &evento);
- 
-            // Se o evento foi de movimentação do mouse
-            if (evento.type == ALLEGRO_EVENT_MOUSE_AXES){
-                // Verifica se está sobre a região do START
-                if (evento.mouse.x >= 255 - al_get_bitmap_width(start)  && evento.mouse.x <= 255 + al_get_bitmap_width(start) &&
-                    evento.mouse.y >= 0 - al_get_bitmap_height(start)  && evento.mouse.y <= 0 + al_get_bitmap_height(start) ){
-                    na_start = 1;
-                }
-                else{
-                    na_start = 0;
-                }
-            }
-            //Senao... verifica se a interacao foi um clique do mouse
-            else if (evento.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP){
-                if (evento.mouse.x >= LARGURA_TELA /*local de clique do mouse*/ - al_get_bitmap_width(sair) - 10 &&
-                    evento.mouse.x <= LARGURA_TELA /*local de clique do mouse*/ - 10 && evento.mouse.y <= ALTURA_TELA - 10 &&
-                    evento.mouse.y >= ALTURA_TELA /*local de clique do mouse*/ - al_get_bitmap_height(sair) - 10){
-                    sair = 0;
-                }
+
+    while (1){    
+        // Espera por um evento
+        ALLEGRO_EVENT evento;
+        al_wait_for_event(interacao, &evento);
+
+        // Se for um evento do tipo clique, vê a posição.
+        if (evento.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP){
+            // Clique no botão sair.
+            if (evento.mouse.x >= 380 && evento.mouse.x <= al_get_bitmap_width(sair) + 380 &&
+                evento.mouse.y >= 400 && evento.mouse.y <= al_get_bitmap_height(sair) + 400) {
+                printf("\nParando a execução...");
+                break;
+            } 
+            // Clique no botão start.
+            else if (evento.mouse.x >= 255  && evento.mouse.x <= 255 + al_get_bitmap_width(start) &&
+                    evento.mouse.y >= 0  && evento.mouse.y <= al_get_bitmap_height(start)) {
+                na_start = 1;
+                printf("\nJogador clicou no start!");
+                continue;
+            } else {
+                printf("\nEvento não suportado.");
+                continue;
             }
         }
+        else if (evento.type == ALLEGRO_EVENT_MOUSE_ENTER_DISPLAY) {
+            printf("\nMouse entrou no display.");
+        }
+        else if (evento.type == ALLEGRO_EVENT_MOUSE_LEAVE_DISPLAY) {
+            printf("\nMouse saiu do display.");
+        }
 
-        // Atualiza a tela
-        al_flip_display();
+        //if (al_is_event_queue_empty(interacao) == 0) {
+            // Atualiza a tela
+            al_flip_display();            
+        //}
     }
- 
+
     // Segura a execução
-    al_rest(100.0);
+    al_rest(5.0);
+    printf("\nFim da execução!\n");
  
     // Finaliza a janela
     al_destroy_display(janela);
