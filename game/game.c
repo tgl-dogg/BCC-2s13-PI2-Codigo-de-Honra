8de5c5c022560140e69ff30e4c733bbd95b4749d
Paulo Henrique Leite <paulo-http@live.com>
2013-09-27 17:59:06 -0300

    editado game.c

--- game/game.c
+++ game/game.c
@@ -9,13 +9,19 @@ const int ALTURA_TELA = 600;
 int main(void){
     // Variável representando a janela principal
     ALLEGRO_DISPLAY *janela = NULL;
-    // Variável representando a imagem (MENU)
-    ALLEGRO_BITMAP *f0 = NULL, *start = NULL, *sair = NULL;
-    // Variável representando a imagem (ANIMAIS)
-    ALLEGRO_BITMAP *dog = NULL, *cat = NULL, *pig = NULL;
-    // Variável representando a imagem (FASE)
-    ALLEGRO_BITMAP *f1 = NULL, *next = NULL, *back = NULL, *opc1 = NULL, *opc2 = NULL, *opc3 = NULL;
-    // Variável representando interação de eventos
+    // Variável representando as imagens (background)
+    ALLEGRO_BITMAP *b01 = NULL, *b02 = NULL;
+    // Variável representando as imagens (menu)
+    ALLEGRO_BITMAP *start = NULL, *leave = NULL, *credits = NULL;
+    // Variável representando as imagens (animale)
+    ALLEGRO_BITMAP  *reptile = NULL, *snake = NULL, *skull = NULL, *waps = NULL, *spider = NULL, *bug = NULL;
+    // Variável representando as imagens (boss)
+    ALLEGRO_BITMAP *demon = NULL, *hydra = NULL, *phantom = NULL, *dragon = NULL;
+    // Variável representando as imagens (actions)
+    ALLEGRO_BITMAP *atk = NULL, *atk_2x = NULL, *run = NULL, *def = NULL; 
+    ALLEGRO_BITMAP *healer = NULL, *book = NULL, *potion = NULL, *door = NULL, *key = NULL;
+    ALLEGRO_BITMAP *repetir = NULL, *next = NULL, *se = NULL, *map = NULL, *help = NULL;
+    // Variável representando as interações de eventos
     ALLEGRO_EVENT_QUEUE *interacao = NULL;
 
     // Inicializa a Allegro
@@ -42,18 +48,19 @@ int main(void){
     }
 
     // Carrega a imagem
-    f0 = al_load_bitmap("resources/wallpapper/f0.png");
-    start = al_load_bitmap("resources/buttons/start.png");
-    sair = al_load_bitmap("resources/buttons/sair.png");
+    b01 = al_load_bitmap("resources/background/b01.png");
+    start = al_load_bitmap("resources/menu/start.png");
+    leave = al_load_bitmap("resources/menu/exit.png");
 
     // Desenha a imagem na tela
-    al_draw_bitmap(f0, 0, 0, 0);    //x, y, z(inverte imagem)
+    al_draw_bitmap(b01, 0, 0, 0);    //x, y, z(inverte imagem)
     al_draw_bitmap(start, 255, 0, 0); //x, y, z(inverte imagem)
-    al_draw_bitmap(sair, 380, 400, 0); //x, y, z(inverte imagem)
-   
+    al_draw_bitmap(leave, 380, 400, 0); //x, y, z(inverte imagem)
+
     // Atualiza a tela
     al_flip_display();
 
+    // Criando interações do mouse
     interacao = al_create_event_queue();
     if (!interacao){
         fprintf(stderr, "Falha ao inicializar a interacao.\n");
@@ -64,52 +71,52 @@ int main(void){
     // Inicialização das interações do mouse
     al_register_event_source(interacao, al_get_mouse_event_source());
     
-    int na_start= 0;
+    int clique = 0;
 
-    while (na_start = 1){    
+    while (1){    
         // Espera por um evento
         ALLEGRO_EVENT evento;
         al_wait_for_event(interacao, &evento);
 
         // Se for um evento do tipo clique, vê a posição do clique.
         if (evento.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP){
-            // Clique no botão sair.
-            if (evento.mouse.x >= 380 && evento.mouse.x <= al_get_bitmap_width(sair) + 380 &&
-                evento.mouse.y >= 400 && evento.mouse.y <= al_get_bitmap_height(sair) + 400) {
+            // Clique no botão leave.
+            if (evento.mouse.x >= 380 && evento.mouse.x <= al_get_bitmap_width(leave) + 380 &&
+                evento.mouse.y >= 400 && evento.mouse.y <= al_get_bitmap_height(leave) + 400) {
                 printf("\nParando a execução...");
                 break;
             } 
             // Clique no botão start.
             else if (evento.mouse.x >= 255  && evento.mouse.x <= 255 + al_get_bitmap_width(start) &&
                     evento.mouse.y >= 0  && evento.mouse.y <= al_get_bitmap_height(start)) {
-                printf("\nJogador clicou no start!");
-                na_start = 0;
-                al_destroy_display(janela);
-                janela = al_create_display(LARGURA_TELA, ALTURA_TELA);
-                al_set_window_title(janela, "FASE1!");
+                printf("\nCarregando a primeira fase...");
                 
-                f1 = al_load_bitmap("resources/wallpapper/f1.png");
-                opc1 = al_load_bitmap("resources/buttons/opc1.png");
-                opc2 = al_load_bitmap("resources/buttons/opc2.png");
-                back = al_load_bitmap("resources/buttons/back.png");
+                //Atribui 1, desta forma valida o if abaixo e inicializa o jogo 
+                clique = 1;
+                // primeira fase
+                if(clique == 1){
+                    al_destroy_display(janela);
+                    janela = al_create_display(LARGURA_TELA, ALTURA_TELA);
+                    al_set_window_title(janela, "FASE1!");
+                
+                    b01 = al_load_bitmap("resources/background/b01.png");
 
-                al_draw_bitmap(f1, 0, 0, 0);
-                al_draw_bitmap(opc1, 230, 200, 0);
-                al_draw_bitmap(opc2, 430, 200, 0);
-                al_draw_bitmap(back, 330, 400, 0);
-                al_flip_display();
-                al_rest(10.0);
-                continue;
+                    al_draw_bitmap(b01, 0, 0, 0);
+                    al_flip_display();
+                    al_rest(100.0);
+                    break;
+                }
+
             } else {
                 printf("\nEvento não suportado.");
                 continue;
             }
         }
         else if (evento.type == ALLEGRO_EVENT_MOUSE_ENTER_DISPLAY) {
-            printf("\nMouse entrou no display.");
+            printf("\nMouse saiu do display.");
         }
         else if (evento.type == ALLEGRO_EVENT_MOUSE_LEAVE_DISPLAY) {
-            printf("\nMouse saiu do display.");
+            printf("\nMouse entrou no display.");
         }
         // Atualiza a tela
         al_flip_display();            
