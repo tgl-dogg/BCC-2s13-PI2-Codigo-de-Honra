605c01ba897ee517d9e03886b7ba5bde8522b515
Paulo Henrique Leite <paulo-http@live.com>
2013-11-20 21:40:31 -0200

    modificado dialog.c

--- game/src/geral/dialog.c
+++ game/src/geral/dialog.c
@@ -5,51 +5,50 @@
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
 
-#define TAM 15
+#define TAM 2
+#define MAX 25
 
-// Atributos da tela
-const int LARGURA_TELA = 300;
-const int ALTURA_TELA = 200;
-    
+static    ALLEGRO_DISPLAY_MODE disp_data;
+static    ALLEGRO_BITMAP *bnew1 = NULL, *bnew2 = NULL, *pos_cond1, *pos_cond2;
+static    ALLEGRO_EVENT_QUEUE *reg_ev = NULL;
+static    ALLEGRO_EVENT ev;
+static    ALLEGRO_COLOR font_color;
+static    ALLEGRO_FONT *font;
+
 // Variável representando a display principal
 ALLEGRO_DISPLAY *display = NULL;
 
 void copy_str(char dest[], char orig[]){
     int i;
-
-    for(i=0 ; i < TAM; i++){
+ 
+    for(i=0 ; i < MAX; i++){
         dest[i] = orig[i];
     }
 }
+ 
+void print_cond(ALLEGRO_FONT *font, ALLEGRO_COLOR font_color, char cond1[][MAX], char cond2[][MAX]){
+    int i, j;
+    char string1[MAX];
+    char string2[MAX];
 
-void print_text(ALLEGRO_FONT *font, ALLEGRO_COLOR font_color, char text[TAM]){
-    int i;
-    char string[TAM];
-    
-    for(i=0; i<2; i++){
-        copy_str(string, text);
-        al_draw_text(font, font_color, 0, i*22, 0, string);
+    pos_cond1 = al_create_sub_bitmap(al_get_backbuffer(display), 30, 20, 370, 70);
+    al_set_target_bitmap(pos_cond1);    
+    for(i=0; i<TAM; i++){
+        copy_str(string1, cond1[i]);
+        al_draw_text(font, font_color, 0, i*22, 0, string1);
     }
 
+    pos_cond2 = al_create_sub_bitmap(al_get_backbuffer(display), 30, 70, 370, 140);
+    al_set_target_bitmap(pos_cond2);   
+    for(j=0; j<TAM; j++){
+        copy_str(string2, cond2[j]);
+        al_draw_text(font, font_color, 0, j*22, 0, string2);
+    }
+ 
     al_flip_display();
 }
 
-void print_bname(ALLEGRO_FONT *font, ALLEGRO_COLOR font_color, char text[TAM]){
-    int i;
-    char string[TAM];
-    
-    copy_str(string, text);
-    al_draw_text(font, font_color, 0, 0, 0, string);
-    al_flip_display();
-}
-
-int dialog(char text[TAM], char bname1[TAM], char bname2[TAM]) {
-    ALLEGRO_DISPLAY_MODE disp_data;
-    ALLEGRO_BITMAP *bnew1 = NULL, *bnew2 = NULL, *pos_text, *pos_bname1, *pos_bname2;
-    ALLEGRO_EVENT_QUEUE *reg_ev = NULL;
-    ALLEGRO_EVENT ev;
-    ALLEGRO_COLOR font_color;
-    ALLEGRO_FONT *font;
+int load_cond(char cond1[][MAX], char cond2[][MAX]) {
 
     al_init();
     al_init_image_addon();
@@ -58,14 +57,14 @@ int dialog(char text[TAM], char bname1[TAM], char bname2[TAM]) {
     al_get_display_mode(0, &disp_data);
 
     // Configura o display
-    display = al_create_display(LARGURA_TELA, ALTURA_TELA);
+    display = al_create_display(400, 250);
     if (!display){
         fprintf(stderr, "Falha ao criar display!\n");
         return -1;
     }
     
     // Atribui onde o display será posicionado
-    al_set_window_position(display, (disp_data.width-LARGURA_TELA)/2, (disp_data.height-ALTURA_TELA)/2);
+    al_set_window_position(display, (disp_data.width-400)/2, (disp_data.height-250)/2);
     // Configura o título do display
     al_set_window_title(display, "NOME DA JANELA!");
 
@@ -83,31 +82,26 @@ int dialog(char text[TAM], char bname1[TAM], char bname2[TAM]) {
         return -1;
     }
 
-    font_color = al_map_rgb(0, 0, 0);
+    font_color = al_map_rgb(255, 255, 255);
     font = al_load_ttf_font("../../res/font/architectsdaughter.ttf", 20, 0);
     if(!font){
         printf("Falha ao carregar architectsdaughter.ttf");
         return 0;
     }
-    
+
+    // carrega as imagens
     bnew1 = al_load_bitmap("../../res/img/dialog/bnew.png");
     bnew2 = al_load_bitmap("../../res/img/dialog/bnew.png");
-    pos_text = al_create_sub_bitmap(al_get_backbuffer(display), 50, 25, 210, 100);
-    pos_bname1 = al_create_sub_bitmap(al_get_backbuffer(display), 45, 135, 125, 200);
-    pos_bname2 = al_create_sub_bitmap(al_get_backbuffer(display), 195, 135, 175, 200);
-    
-    // desenha imagens
-    al_clear_to_color(al_map_rgb(255, 255, 255));
-    al_draw_bitmap(bnew1, 25, 125, 0);
-    al_draw_bitmap(bnew2, 175, 125, 0);
+
+    // desenha as imagens
+    al_clear_to_color(al_map_rgb(0, 0, 0));
+    al_draw_bitmap(bnew1, 60, 175, 0);
+    al_draw_bitmap(bnew2, 240, 175, 0);
 
     // desenha texto em tela
-    al_set_target_bitmap(pos_text);
-    print_text(font, font_color, text);
-    al_set_target_bitmap(pos_bname1);
-    print_bname(font, font_color, bname1);
-    al_set_target_bitmap(pos_bname2);    
-    print_bname(font, font_color, bname2);
+    al_draw_text(font, font_color, 75, 180, 0, "cond 1");
+    al_draw_text(font, font_color, 250, 180, 0, "cond 2");
+    print_cond(font, font_color, cond1, cond2);
 
     al_flip_display();
 
@@ -126,15 +120,15 @@ int dialog(char text[TAM], char bname1[TAM], char bname2[TAM]) {
         
         if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP){
             // clique no botão1
-            if (ev.mouse.x >= 25 && ev.mouse.x <= al_get_bitmap_width(bnew1) + 25 &&
-                ev.mouse.y >= 125 && ev.mouse.y <= al_get_bitmap_height(bnew1) + 125) {
-                return -1;
+            if (ev.mouse.x >= 60 && ev.mouse.x <= al_get_bitmap_width(bnew1) + 60 &&
+                ev.mouse.y >= 175 && ev.mouse.y <= al_get_bitmap_height(bnew1) + 175) {
+                return 0;
             } // clique no botão2 
-            else if (ev.mouse.x >= 175  && ev.mouse.x <= al_get_bitmap_width(bnew2) + 175 &&
-                     ev.mouse.y >= 125  && ev.mouse.y <= al_get_bitmap_height(bnew2) + 125) {
+            else if (ev.mouse.x >= 240  && ev.mouse.x <= al_get_bitmap_width(bnew2) + 240 &&
+                     ev.mouse.y >= 175  && ev.mouse.y <= al_get_bitmap_height(bnew2) + 175) {
                     // desintegra as interações atuais
                     al_unregister_event_source(reg_ev, al_get_mouse_event_source());
-                    return 0;
+                    return 1;
             } else {
                 printf("\nEvento não suportado.\n");
                 continue;
@@ -152,11 +146,4 @@ int dialog(char text[TAM], char bname1[TAM], char bname2[TAM]) {
     al_destroy_display(display);
 
     return 0;
-}
-
-int main() {
-    char txt[TAM]={"012345678912345"};
-    char bt1[TAM]={"false"}, bt2[TAM]={"true"};
-
-    dialog(txt, bt1, bt2);
 }
--- game/src/geral/dialog.h
+++ game/src/geral/dialog.h
@@ -1,9 +1,20 @@
 #ifndef _DIALOG_H_
 #define _DIALOG_H_
 
+#include <stdio.h>
+
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+
+#define TAM 2
+#define MAX 25
+
 void copy_str(char dest[], char orig[]);
-void print_text(ALLEGRO_FONT *font, ALLEGRO_COLOR font_color, char text[TAM]);
-void print_bname(ALLEGRO_FONT *font, ALLEGRO_COLOR font_color, char text[TAM]);
-int	dialog(char text[TAM], char bname1[TAM], char bname2[TAM]);
+
+void print_cond(ALLEGRO_FONT *font, ALLEGRO_COLOR font_color, char cond1[][MAX], char cond2[][MAX]);
+
+int load_cond(char cond1[][MAX], char cond2[][MAX]);
 
 #endif
