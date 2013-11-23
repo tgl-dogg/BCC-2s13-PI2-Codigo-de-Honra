074d409702e553fa7eb176f144b3f45bf35c5886
root <vinidoggll@hotmail.com>
2013-11-22 22:18:51 -0200

    Alocação das imagens movida para o módulo de fases

--- game/src/fases/desafios/desafios.c
+++ game/src/fases/desafios/desafios.c
@@ -13,19 +13,29 @@ void click_action_buttons(clk_flag flags, challenger_rule cr, card_pile *cp);
 
 void execute_event(int ev_type, clk_flag flags, challenger_rule cr, card_pile *cp);
 
-// Variável externa
+// Variável externa de janela
 extern ALLEGRO_DISPLAY *janela;
 
-// Arrays globais da área de seleção do usuário
-static int img_size = 5;
-static ALLEGRO_BITMAP *im_prog_set[5];
-static ALLEGRO_BITMAP *im_cond_set[5];
-static ALLEGRO_BITMAP *im_act_set[5];
+// Background
+extern ALLEGRO_BITMAP im_bg;
+
+// Botões
+extern ALLEGRO_BITMAP im_help;
+extern ALLEGRO_BITMAP im_memory;
+extern ALLEGRO_BITMAP im_compile;
+
+// Cartas
+extern ALLEGRO_BITMAP *im_prog_set[5];
+extern ALLEGRO_BITMAP *im_cond_set[5];
+extern ALLEGRO_BITMAP *im_act_set[5];
 
 /* Cria o desafio de acordo com a regra de desafio (challenger_rule). */
 int create_desafio(challenger_rule cr){
+    // TODO rever o challenger_rule struct
+
     // Guarda o resultado da nossa operação.
-    int resultado = 0, i;
+    int resultado = 0;
+
     // Cria flag para controle dos eventos de clique.
     clk_flag ev_flag = {-1, -1, -1, -1};   
     // Cria pilha de cartas.
@@ -34,46 +44,18 @@ int create_desafio(challenger_rule cr){
     // Nova event queue para registrar os eventos desta janela.
     ALLEGRO_EVENT_QUEUE *ev_queue = NULL;
 
-   	// Evento para os novos cliques.
-	ALLEGRO_EVENT ev_click;
+    // Evento para os novos cliques.
+    ALLEGRO_EVENT ev_click;
+    
+    // Desenha o background.
+    al_draw_bitmap(im_bg, 0, 0, 0);
 
-	// Background.
-	ALLEGRO_BITMAP *bg = NULL;
-
-	// Botões.
-	ALLEGRO_BITMAP *im_help = NULL;
-	ALLEGRO_BITMAP *im_memory = NULL;
-	ALLEGRO_BITMAP *im_compile = NULL;  
-   	
-   	bg = al_load_bitmap("res/img/bg/bg_test.png");
-    al_draw_bitmap(bg, 0, 0, 0);
-
- 	im_help = al_load_bitmap("res/img/gen/help.png");
-	im_memory = al_load_bitmap("res/img/gen/memory.png");
-	im_compile = al_load_bitmap("res/img/prog/compile_symbol.png");
-	
-    im_prog_set[0] = al_load_bitmap("res/img/prog/direct_symbol.png");
-    im_prog_set[1] = al_load_bitmap("res/img/prog/if_symbol.png");    
-    im_prog_set[2] = al_load_bitmap("res/img/prog/while_symbol.png");
-    im_prog_set[3] = im_prog_set[4] = NULL;
-
-    im_cond_set[0] = al_load_bitmap("res/img/prog_condition/if_condition.png");    
-    im_cond_set[1] = al_load_bitmap("res/img/prog_condition/if_else.png");
-    im_cond_set[2] = al_load_bitmap("res/img/prog_condition/while_break.png");
-    im_cond_set[3] = al_load_bitmap("res/img/prog_condition/array_symbol.png");
-    im_cond_set[4] = NULL;
-	
-    im_act_set[0] = al_load_bitmap("res/img/act/atk.png");    
-    im_act_set[1] = al_load_bitmap("res/img/act/def.png");
-    im_act_set[2] = al_load_bitmap("res/img/act/atk_double.png");
-    im_act_set[3] = al_load_bitmap("res/img/act/grab.png");
-    im_act_set[4] = al_load_bitmap("res/img/act/array_item.png");
-
-	// Desenha as cartas na tela.
+    // Desenha os botões.
     draw_help_card(im_help);
     draw_memory_card(im_memory);
     draw_compile_card(im_compile);
 
+    // Desenha as cartas.
     draw_prog_cards(im_prog_set, cr);    
     draw_conditional_cards(im_cond_set, cr);
     draw_action_cards(im_act_set, cr);
@@ -84,29 +66,30 @@ int create_desafio(challenger_rule cr){
     card_pile_init(&card_set);
 
     // Cria uma event queue diferente da tela principal.
-	ev_queue = al_create_event_queue();
-	// Registra a interação com o mouse novamente.
-	al_register_event_source(ev_queue, al_get_mouse_event_source());
+    ev_queue = al_create_event_queue();
+    // Registra a interação com o mouse novamente.
+    al_register_event_source(ev_queue, al_get_mouse_event_source());
     
-
     while(1){    
-    	int ev_type = -1;	
-    	al_wait_for_event(ev_queue, &ev_click);
+        int ev_type = -1;   
+        al_wait_for_event(ev_queue, &ev_click);
 
+        // Detecta o tipo de evento.
         if (ev_click.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP){
-        	// detecta o tipo de evento e seta as flags.
-        	ev_type = detect_click_pos(ev_click, &ev_flag, cr);
+            // Detecta onde foi o clique do usuário e seta as flags referentes.
+            ev_type = detect_click_pos(ev_click, &ev_flag, cr);
 
-        	//executa o evento especificado.
-        	execute_event(ev_type, ev_flag, cr, &card_set);
+            //Executa o evento registrado na flag.
+            execute_event(ev_type, ev_flag, cr, &card_set);
+
             al_flip_display();  
-        	
-        	// Se o usuário clicou em compile ou reset, por enquanto finalizamos os eventos.
-        	if(ev_type == 6 || ev_type == 7){
-        		al_unregister_event_source(ev_queue, al_get_mouse_event_source());
+            
+            // Se o usuário clicou em compile ou reset, por enquanto finalizamos os eventos.
+            if(ev_type == 6 || ev_type == 7){
+                al_unregister_event_source(ev_queue, al_get_mouse_event_source());
                 printf("Chegou no compile/reset!\n");
-        		break;
-        	}
+                break;
+            }
         }
     }
 
@@ -115,96 +98,98 @@ int create_desafio(challenger_rule cr){
     return resultado;
 }
 
-// Clique nas cartas de programação.
+/* Clique nas cartas de programação. */
 void click_prog_buttons(clk_flag flags, challenger_rule cr, card_pile *cp){
     // Verifica se o status está ok (== 1).
-	if(flags.ev_status == 1){
+    if(flags.ev_status == 1){
         // verifica o "nível de carta" em programação.
         // para saber se o usuário pode utilizar a posição em que clicou.
         if(flags.card_pos < cr.prog){
             // Adiciona a carta à pilha de cartas do usuário de acordo com
-            // a nossa definição de index de cartas.
+            // a nossa definição de index de cartas (cartas de programação começam do 0).
             card_pull(cp, flags.card_num + 0);            
     
             // Desenha ela na área de cartas selecionadas.
             draw_selected_cards(im_prog_set[flags.card_pos], flags.card_num);  
         }
-	}
+    }
 }
 
-// Clique nas cartas de condição.
+/* Clique nas cartas de condição. */
 void click_condition_buttons(clk_flag flags, challenger_rule cr, card_pile *cp){
-	if(flags.ev_status == 1){
+    if(flags.ev_status == 1){
         // verifica o "nível de carta" em programação.
         // para saber se o usuário pode utilizar a posição em que clicou.
         if(flags.card_pos < cr.cond){
             // Adiciona a carta à pilha de cartas do usuário de acordo com
-            // a nossa definição de index de cartas.
+            // a nossa definição de index de cartas (cartas de condição começam do 1000).
             card_pull(cp, flags.card_num + 1000);
 
             // Desenha ela na área de cartas selecionadas.
             draw_selected_cards(im_cond_set[flags.card_pos], flags.card_num);
         }
-	}
+    }
 }
 
-// Clique nas cartas de ação.
+/* Clique nas cartas de ação. */
 void click_action_buttons(clk_flag flags, challenger_rule cr, card_pile *cp){
-	if(flags.ev_status == 1){      
+    if(flags.ev_status == 1){      
         // verifica o "nível de carta" em programação.
         // para saber se o usuário pode utilizar a posição em que clicou.
         if(flags.card_pos < cr.act){
             // Adiciona a carta à pilha de cartas do usuário de acordo com
-            // a nossa definição de index de cartas.
+            // a nossa definição de index de cartas (cartas de ação começam do 2000).
             card_pull(cp, flags.card_num + 2000);
 
             // Desenha ela na área de cartas selecionadas.
             draw_selected_cards(im_act_set[flags.card_pos], flags.card_num);  
         }
-	}
+    }
 }
 
-// depois de detectar o evento, executa o mesmo de acordo com o tipo e as flags.
+/* Executa o mesmo de acordo com o tipo e as flags. */
 void execute_event(int ev_type, clk_flag flags, challenger_rule cr, card_pile *cp){
-	switch(ev_type){
-        // Cartas de programação.
-		case 1:
-			click_prog_buttons(flags, cr, cp);
-			break;
+    switch(ev_type){
+        /* Cartas de programação. */
+        case 1:
+            click_prog_buttons(flags, cr, cp);
+            break;
 
-        // Cartas de condição.
-		case 2:
-			click_condition_buttons(flags, cr, cp);
-			break;
+        /* Cartas de condição. */
+        case 2:
+            click_condition_buttons(flags, cr, cp);
+            break;
 
-        // Cartas de ação.
-		case 3:
-			click_action_buttons(flags, cr, cp);
-			break;
+        /* Cartas de ação. */
+        case 3:
+            click_action_buttons(flags, cr, cp);
+            break;
 
-        // Carta de memória.
-		case 4:
+        /* Carta de memória. */
+        case 4:
             // mostrar instruções.
-			break;	
+            break;  
 
-        // Carta de ajuda
-		case 5:
+        /* Carta de ajuda */
+        case 5:
             // mostrar ajuda.
-			break;
+            break;
 
-        // Carta de compilar.
-		case 6:
-            if((validate_selection(cr.v, (*cp).v)) == -1){
-                //GGWP return -1;
-            } else {
-                //deubosta return indice da carta errada == 0;
+        /* Carta de compilar. */
+        case 6:
+            // TODO melhorar esta validação
+            return validate_selection(cr.v, (*cp).v)
+            //GGWP return -1;
+            //deubosta return indice da carta errada == 0;
+            break;
+
+        /* Carta de reset. */
+        case 7:
+            // Verifica se a pilha não está vazia e dá um pop nela.
+            // TODO criar sub_bitmap para área de de cartas selecionadas para limpar as seleções erradas do usuário.
+            if (card_stack_count(cp) != 0) {
+                card_pop(cp);
             }
-            // validar 
-			break;
-
-        // Carta de reset.
-		case 7:
-            // criar
-			break;
-	}
+            break;
+    }
 }
--- game/src/fases/fases.c
+++ game/src/fases/fases.c
@@ -1,10 +1,67 @@
 #include "fases.h"
 #include "descricao/descricao.h"
 
+// Pega a variável global da janela.
 extern ALLEGRO_DISPLAY *janela;
 
+// Declara as variáveis globais das cartas.
+ALLEGRO_BITMAP *im_prog_set[3];
+ALLEGRO_BITMAP *im_cond_set[4];
+ALLEGRO_BITMAP *im_act_set[5];
+
+// Background.
+ALLEGRO_BITMAP *im_bg;
+
+// Botões.
+ALLEGRO_BITMAP *im_help;
+ALLEGRO_BITMAP *im_memory;
+ALLEGRO_BITMAP *im_compile;  
+
 /* Inicialização das fases no geral. */
-void fases_init(){
-	load_description_f1(janela);
+void fases_init() {
+	// Carrega na memória em partes, pegando apenas o que será utilizado em cada desafio.
+    
+   	// Botões (estarão em todas as fases, então já carrega de uma vez)
+ 	im_help = al_load_bitmap("res/img/gen/help.png");
+	im_memory = al_load_bitmap("res/img/gen/memory.png");
+	im_compile = al_load_bitmap("res/img/prog/compile_symbol.png");
+
+	/* Fase 1 */
+	// Background
+   	bg = al_load_bitmap("res/img/bg/bg_test.png");
+
+   	// Cartas
+    im_prog_set[0] = al_load_bitmap("res/img/prog/direct_symbol.png");
+    im_prog_set[1] = al_load_bitmap("res/img/prog/if_symbol.png");  
+
+    im_cond_set[0] = al_load_bitmap("res/img/prog_condition/if_condition.png");    
+    im_cond_set[1] = al_load_bitmap("res/img/prog_condition/if_else.png");
+
+    im_act_set[0] = al_load_bitmap("res/img/act/atk.png");    
+    im_act_set[1] = al_load_bitmap("res/img/act/def.png");
+
+    // File
+    // TODO load txt
+    load_description_f1(janela);
     fase1_init();
+
+    /* Fase 2 */
+    im_prog_set[2] = al_load_bitmap("res/img/prog/while_symbol.png");
+
+    im_cond_set[2] = al_load_bitmap("res/img/prog_condition/while_break.png");
+
+    im_act_set[2] = al_load_bitmap("res/img/act/atk_double.png");
+
+    // TODO load txt
+    // carrega fase 2
+    fase2_init();
+
+    /* Fase 3 */
+    im_cond_set[3] = al_load_bitmap("res/img/prog_condition/array_symbol.png");
+	
+    im_act_set[3] = al_load_bitmap("res/img/act/grab.png");
+    im_act_set[4] = al_load_bitmap("res/img/act/array_item.png");
+
+    // TODO load txt.
+    fase3_init();
 }
