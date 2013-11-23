d0ef66326b32639441e2fe2867c5f76b9dfcc8ff
root <vinidoggll@hotmail.com>
2013-11-23 12:58:07 -0200

    Criada a primeira estrutura de texto com desafio (incompleto)

--- game/src/fases/fase1.c
+++ game/src/fases/fase1.c
@@ -13,6 +13,14 @@ extern ALLEGRO_BITMAP *im_bg;
 extern ALLEGRO_BITMAP *im_boss;
 extern ALLEGRO_BITMAP *im_mon_set[5];
 
+// Cartas
+extern ALLEGRO_BITMAP *im_prog_set[5];
+extern ALLEGRO_BITMAP *im_cond_set[5];
+extern ALLEGRO_BITMAP *im_act_set[5];
+
+// Tutor
+extern ALLEGRO_BITMAP *im_tutor;
+
 // Text file.
 extern char ***text;
 
@@ -38,19 +46,48 @@ int fase1_init() {
     // Desenha o background.
     al_draw_bitmap(im_bg, 0, 0, 0);
 
+    // Define as cartas a serem desenhadas.
+    cr.prog = 1;
+    cr.cond = 0;
+    cr.act = 1;
+
+    draw_prog_cards(im_prog_set, cr);    
+    draw_conditional_cards(im_cond_set, cr);
+    draw_action_cards(im_act_set, cr);
+
+    // Texto de introdução sobre as primeiras cartas
+    speaker = im_tutor;
+	text_pos = 0;
+    draw_text(text, text_pos, speaker);
+
+    // Texto de explicação de como usar as cartas
+	text_pos++;
+    draw_text(text, text_pos, speaker);
+
+    // Primeiro desafio
+    text_pos++;
+    speaker = im_mon_set[0];
+    draw_text(text, text_pos, speaker);
+
+    create_desafio(cr);
+
+    return 0;
+
+    text_pos++;
+    speaker = im_tutor;
     // TODO definir textos a serem mostrados, a regra do desafio, o speaker, etc...
     cr.prog = 2;
     cr.cond = 2;
     cr.act = 2;
+    //cr.v = 
 
-    speaker = tutor;
-    speaker = im_mon_set[0];
-    speaker = im_mon_set[1];
-    speaker = im_mon_set[2];
-    speaker = boss;
+    //speaker = tutor;
+    //speaker = im_mon_set[0];
+    //speaker = im_mon_set[1];
+    //speaker = im_mon_set[2];
+    //speaker = boss;
 
 	// texto texto texto
-	text_pos = 0;
 	draw_text(text, text_pos, speaker);
 
     // Cria desafio de acordo com as regras especificadas.
