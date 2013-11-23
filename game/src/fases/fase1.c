14ca3cafd97d59eb7b5aa1efbf7031b96befb6eb
root <vinidoggll@hotmail.com>
2013-11-23 03:30:24 -0200

    Estruturação final da fase 1 (incompleto)

--- game/src/fases/fase1.c
+++ game/src/fases/fase1.c
@@ -1,26 +1,61 @@
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_image.h>
-
+#include "fases.h"
 #include "desafios/desafios.h"
+#include "descricao/descricao.h"
 #include "../geral/cards.h"
 
-void fase1_init(){
-	int i;
-	ALLEGRO_COLOR color = al_map_rgb(255, 255, 255);
+// Variável externa de janela.
+extern ALLEGRO_DISPLAY *janela;
 
-	//struct que controla o desafio
+// Background.
+extern ALLEGRO_BITMAP *im_bg;
+
+// Monstros.
+extern ALLEGRO_BITMAP *im_boss;
+extern ALLEGRO_BITMAP *im_mon_set[5];
+
+// Text file.
+extern char ***text;
+
+int fase1_init() {
+	// Guarda qual texto do file queremos utilizar;
+	int text_pos;
+
+	// Cor para limpar a tela
+	ALLEGRO_COLOR clean_color;
+
+	// Imagem de quem está falando com o jogador;
+	ALLEGRO_BITMAP *speaker;
+
+	// Struct que controla o desafio
 	challenger_rule cr;	
+	// Inicializa a struct dos desafios
+	cr_init(&cr);
+	
+	// Define a cor para limpar a tela (preto) e limpa.
+ 	clean_color = al_map_rgb(0, 0, 0);
+    al_clear_to_color(clean_color);
 
-	cr.prog = 3;
-	cr.cond = 4;
-	cr.act = 5;
+    // Desenha o background.
+    al_draw_bitmap(im_bg, 0, 0, 0);
 
-	for(i = 0; i < 15; i++){
-		cr.v[i] = -1;
-	}
+    // TODO definir textos a serem mostrados, a regra do desafio, o speaker, etc...
+    cr.prog = 2;
+    cr.cond = 2;
+    cr.act = 2;
 
-    al_clear_to_color(color);
+    speaker = tutor;
+    speaker = im_mon_set[0];
+    speaker = im_mon_set[1];
+    speaker = im_mon_set[2];
+    speaker = boss;
 
+	// texto texto texto
+	text_pos = 0;
+	draw_text(text, text_pos, speaker);
+
+    // Cria desafio de acordo com as regras especificadas.
     create_desafio(cr);
     al_flip_display();
+
+    return 0;
 }
